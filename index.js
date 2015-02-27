5701a0955a87d611b85f08c0f53bd871174dbe93
Prashant Balan <prashant.balan@hotmail.com>
2015-02-28 01:48:58 +0800

    social network class with Facebook post message

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ var options =  {
                };
 
 function app(req, res) {
-
-	res.write();
+	res.write('app '+app);
 	res.end();
 };
 
@@ -27,9 +26,33 @@ server
   .use(restify.bodyParser())
   .use(restify.queryParser());
 
+server.timeout = 0;
+
 server.listen(options.port, function(request, response){
 
 	// server created
 	console.log('server running at http://' + options.host + '/ on port ' + server.address().port);
 });
 
+server.get('/:network/:type/:message', function (req, res, next) {
+  // create report module
+  if(req.params.network===undefined || req.params.network === null)
+  {
+  	res.send('network is required');
+  }
+  if(req.params.type === undefined  || req.params.type === null)
+  {
+  	res.send('type is required');
+  }
+  if(req.params.message === undefined  || req.params.message === null)
+  {
+  	res.send('message is required');
+  }
+  console.log('network: ' + req.params.network + ', type: ' + req.params.type + ', message: ' + req.params.message);
+  // create report module
+  var SocialNetworkObj = new SocialNetwork();
+  SocialNetworkObj.sendMessage(req.params.network, req.params.type, req.params.message);
+  res.send('what you want?');
+});
+
+server.on('data',app);
